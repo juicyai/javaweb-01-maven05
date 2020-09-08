52175a566091c9d0330cb377ad8c852bd1e9e60d
koki123456 <aiwei-juicy@foxmail.com>
2020-09-08 17:11:27 +0800

    Initial commit

---
+++ src/main/java/com/koki/servlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.koki.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+//        String name=req.getParameter("name");
+//        if(name==null){
+//            name="world";
+//        }
+        PrintWriter pw=resp.getWriter();
+        pw.write("<h1>hello,world, I'm fine now"+"!</h1>");
+        pw.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+    }
+}
---
+++ src/main/java/com/koki/servlet/Main.java
@@ -0,0 +1,33 @@
+package com.koki.servlet;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    
+    public static void start() throws LifecycleException, IOException {
+
+        // 启动Tomcat:
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(Integer.getInteger("port", 8080));
+        tomcat.getConnector();
+        // 创建webapp:
+        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(
+                new DirResourceSet(resources, "/WEB-INF/classes", new File("target/classes").getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+    public static void main(String[] args) throws LifecycleException, IOException {
+        start();
+    }
+}
